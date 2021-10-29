9ece214eca48860a90e71f0583e8228c83006704
MatildaDelacourt <matilda.delacourt@gmail.com>
2021-10-29 14:59:19 -0400

    add basic html content

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-/*const http = require('http');
+/*
+const http = require('http');
 const HOSTNAME = process.env.HOSTNAME || 'localhost';
 const PORT = process.env.PORT || 3001;
 
@@ -9,11 +10,11 @@ const server = http.createServer((request, response) => {
 });
 
 server.listen(PORT, HOSTNAME, () => {
-  //  console.log(`server running at http://${HOSTNAME}:${PORT}/`);
+    console.log(`server running at http://${HOSTNAME}:${PORT}/`);
 });
 
-//console.log(__filename);
-//console.log(__dirname);
+console.log(__filename);
+console.log(__dirname);
 
 const { unlink } = require('fs');
 
@@ -23,9 +24,22 @@ unlink('hello.txt', (err) => {
     return;
   }
   console.log('File deleted!');
-});*/
+});
 
 import { addNums } from './addNums.js';
 
 const sum = addNums(2, 2);
 console.log(sum);
+*/
+
+const http = require('http');
+
+const PORT = process.env.PORT || 3001;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World<h1>');
+});
+
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
